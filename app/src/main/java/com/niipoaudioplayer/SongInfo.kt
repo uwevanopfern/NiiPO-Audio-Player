0bcf32ac72ab607d1efbd7368931963dc45ae9a4
UweVanOpfern <uweaime@gmail.com>
2020-12-20 12:40:40 +0200

    Full project

---
+++ app/src/main/java/com/niipoaudioplayer/SongInfo.kt
@@ -0,0 +1,14 @@
+package com.niipoaudioplayer
+
+class SongInfo{
+
+    var Title:String?=null
+    var AuthorName:String?=null
+    var SongURL:String?=null
+    constructor(Title:String,AuthorName:String,SongURL:String){
+        this.Title=Title
+        this.AuthorName=AuthorName
+        this.SongURL=SongURL
+    }
+
+}
